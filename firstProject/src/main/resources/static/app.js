037ddef2ddaa73e861ee56ca70fa2e637ca07607
kye <yds04022@dkitec.com>
2022-02-25 17:40:54 +0900

    angularSetting1

---
+++ firstProject/src/main/resources/static/app.js
@@ -0,0 +1,13 @@
+(function(){
+	'use strict';
+	
+	angular.module('myApp', [])
+	.controller('test', test);
+	
+	function test($scope){
+		console.info("test");
+		
+		$scope.angularTest = "Angular Test Success";
+		
+	}
+})();
