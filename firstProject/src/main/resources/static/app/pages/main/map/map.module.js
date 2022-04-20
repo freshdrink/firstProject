1eec92fe091abe989a78cccb459d28cb04f9f292
kye <yds04022@gmail.com>
2022-04-20 17:59:24 +0900

    2022.04.20 map 마커 및 접속위치정보를 이용한 위치 표시 (https 환경에서 가능)

--- firstProject/src/main/resources/static/app/pages/main/map/map.module.js
+++ firstProject/src/main/resources/static/app/pages/main/map/map.module.js
@@ -20,6 +20,11 @@
 		// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
 		var map = new kakao.maps.Map(mapContainer, mapOption);
 		
+		
+		
+		
+		/** ================ 마커 표출 ==================== * */
+		
 		// 지도를 클릭한 위치에 표출할 마커입니다
 		var marker = new kakao.maps.Marker({ 
 		    // 지도 중심좌표에 마커를 생성합니다 
@@ -27,6 +32,35 @@
 		}); 
 		// 지도에 마커를 표시합니다
 		marker.setMap(map);
+		
+
+		
+		
+		/** ============== html5에서 해당 위치 가져오는것 (geolocation) ==================== * */
+		
+		// HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
+		if (navigator.geolocation) {
+
+		    // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+		    navigator.geolocation.getCurrentPosition(function(position) {
+		        var lat = position.coords.latitude, // 위도
+		            lon = position.coords.longitude; // 경도
+		        
+		        var locPosition = new kakao.maps.LatLng(lat, lon); // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+		        
+		        // 마커 위치를 클릭한 위치로 옮깁니다
+			    marker.setPosition(locPosition);
+			    // 지도 중심을 이동 시킵니다
+			    map.setCenter(locPosition);
+			    $scope.msg(locPosition);
+
+		      });
+		    
+		}
+
+		
+
+		/** ================ 클릭 이벤트 ==================== * */
 
 		// 지도에 클릭 이벤트를 등록합니다
 		// 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
@@ -38,13 +72,71 @@
 		    // 마커 위치를 클릭한 위치로 옮깁니다
 		    marker.setPosition(latlng);
 		    
+		    $scope.msg(latlng);
+
+		});
+		
+		
+		/** ================ 클릭 이벤트 - 위경도 표시 ==================== * */
+		$scope.msg = function(latlng){
+			
 		    var message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, <Br>';
 		    message += '경도는 ' + latlng.getLng() + ' 입니다';
 		    
 		    var resultDiv = document.getElementById('result'); 
 		    resultDiv.innerHTML = message;
-
+		}
+		$scope.msg(mapOption.center);
+		
+		
+		
+		
+		/** ================ 마우스 오버 이벤트 - 인포윈도우 표시 ==================== * */
+		
+		// 마커에 커서가 오버됐을 때 마커 위에 표시할 인포윈도우를 생성합니다
+		var iwContent = '<div style="padding:5px;">Hello World!</div>'; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+		
+		// 인포윈도우를 생성합니다
+		var infowindow = new kakao.maps.InfoWindow({
+		    content : iwContent
 		});
+		
+		// 마커에 마우스오버 이벤트를 등록합니다
+		kakao.maps.event.addListener(marker, 'mouseover', function() {
+		  // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
+		    infowindow.open(map, marker);
+		});
+
+		// 마커에 마우스아웃 이벤트를 등록합니다
+		kakao.maps.event.addListener(marker, 'mouseout', function() {
+		    // 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
+		    infowindow.close();
+		});
+		
+		
+		
+		/** ================ 지도 확대 축소 ==================== * */
+		
+		/*// 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+		$scope.zoomIn = function() {
+		    map.setLevel(map.getLevel() - 1);
+		}
+
+		// 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+		$scope.zoomOut = function() {
+		    map.setLevel(map.getLevel() + 1);
+		}*/
+		
+		// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+		var mapTypeControl = new kakao.maps.MapTypeControl();
+		// 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+		// kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+		map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+		
+		// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+		var zoomControl = new kakao.maps.ZoomControl();
+		map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+		
 
 		
 	}
